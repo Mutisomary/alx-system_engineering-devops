e5808ef275e2e9ce2fec6039911b8f5f310f536b
Mutisomary <marymutso2016@gmail.com>
2023-10-23 05:17:18 -0700

    Gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     to_do = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
     completed = [title.get("title") for title in to_do if
                  title.get('completed') is True]
-    print("Employee {} is done with tasks ({}/{}):".format(user.get("name"),
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
           len(completed), len(to_do)))
     [print("\t {}".format(title)) for title in completed]
